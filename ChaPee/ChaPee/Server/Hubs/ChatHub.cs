0c03f70a35ea9aeec7a753942fee2dec7740c8a4
Slovesson <goodygoodyday@gmail.com>
2020-05-26 01:50:05 +0300

    Loading
    Loading draft project

---
+++ ChaPee/ChaPee/Server/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+//Додаємо концентратор SignalR
+
+namespace ChaPee.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            
+            //для роботи с БД спробувати Items
+
+        }
+    }
+}
